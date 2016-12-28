b5cbcfa443061d54a61b37bd4e6515adc2da5e68
ipcrm <mcadorette@gmail.com>
2016-12-27 22:47:37 -0500

    update

--- src/footer.php
+++ src/footer.php
@@ -99,5 +99,6 @@
 	<?php if(isset($wl_theme_options['custom_css'])) { ?> 
 	<style type="text/css"><?php echo esc_attr($wl_theme_options['custom_css']); ?></style>
   <?php } ?>
+<b>THIS IS A TEST</b>
 </body>
 </html>
